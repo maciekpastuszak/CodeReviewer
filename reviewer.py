450b29a12c8a884c728ac08845ae8ee4e6c6662e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-27 11:08:01 +0200

    init commit

---
+++ reviewer.py
@@ -0,0 +1,4 @@
+PROMPT = """
+You will receive a file's contents as text.
+Generate a code review for the file. Indicate what changes should be made to improve its style, performance, readibility and maintainability. If there are any reputable libraries that could be introduced to improve the code, suggest them. Be kind and constructive. For each suggested change, include line numbers to which you are referring.
+"""
