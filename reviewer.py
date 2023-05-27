644a63a4190f066979238305b1bbd6c290973251
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-27 11:10:20 +0200

    config dotenv

--- reviewer.py
+++ reviewer.py
@@ -1,3 +1,10 @@
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 PROMPT = """
 You will receive a file's contents as text.
 Generate a code review for the file. Indicate what changes should be made to improve its style, performance, readibility and maintainability. If there are any reputable libraries that could be introduced to improve the code, suggest them. Be kind and constructive. For each suggested change, include line numbers to which you are referring.
