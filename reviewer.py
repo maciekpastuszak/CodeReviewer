1318d596d4b2aa6feda9199e019e8a52ab0b185d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-28 11:52:59 +0200

    add messages logic

--- reviewer.py
+++ reviewer.py
@@ -9,3 +9,10 @@ PROMPT = """
 You will receive a file's contents as text.
 Generate a code review for the file. Indicate what changes should be made to improve its style, performance, readibility and maintainability. If there are any reputable libraries that could be introduced to improve the code, suggest them. Be kind and constructive. For each suggested change, include line numbers to which you are referring.
 """
+
+messages = [
+    {"role": "system", "content": PROMPT},
+    {"role": "user", "content": f"Code review the following file: {filecontent}"},
+]
+
+openai.ChatCompletion.create(model="gpt-4", messages=messages)
